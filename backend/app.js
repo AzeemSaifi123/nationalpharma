3f4cd3945d2eb7d53a2bb33113b1ee49c7c6b812
AzeemSaifi123 <adkhan9355@gmail.com>
2025-01-28 22:21:15 +0530

    changes in app.js

--- backend/app.js
+++ backend/app.js
@@ -36,18 +36,18 @@ const server = http.createServer(app);
 server.listen(port);
 
 app.use((req,res,next)=>{
-    // res.setHeader("Access-Control-Allow-Origin", "https://thenationalpharma.com");
-    // res.setHeader(
-    //   "Access-Control-Allow-Headers",
-    //   "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    // );
-    // res.setHeader(
-    //   "Access-Control-Allow-Methods",
-    //   "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    // );
-    res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    // res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+    // res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
