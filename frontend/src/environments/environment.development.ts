9185a75cd7e67235be286a071a67cfd76a064234
AzeemSaifi123 <adkhan9355@gmail.com>
2025-01-28 21:55:36 +0530

    Url update of mailer

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-        apiUrl:"https://api.147.93.96.8:27017/database"
-      // apiUrl:"http://localhost:3000/database"
+        //apiUrl:"https://api.thenationalpharma.com/database"
+        apiUrl:"http://localhost:3000/database"
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl:"https://api.147.93.96.8:27017/database"
-   // apiUrl:"http://localhost:3000/database"
+   // apiUrl:"https://api.thenationalpharma.com/database"
+    apiUrl:"http://localhost:3000/database"
 };
