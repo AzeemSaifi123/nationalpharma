58733400d5cdcb5df3bb205bac8fafe2ca9b4094
Azeem saifi <adkhan9355@gmail.com>
2025-01-29 14:53:48 +0530

    cors setup

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,8 @@ const app = express();
 const http = require("http");
 // const app = require('app');
 
-// const uri = 'mongodb://127.0.0.1:27017/database'; // Replace 'mydatabase' with your database name
-const uri = 'mongodb://147.93.96.8:27017/database'; // Replace 'mydatabase' with your database name
+const uri = 'mongodb://127.0.0.1:27017/database'; // Replace 'mydatabase' with your database name
+// const uri = 'mongodb://147.93.96.8:27017/database'; // Replace 'mydatabase' with your database name
 
 // Connect to MongoDB
 mongoose.connect(uri).then(() => {
