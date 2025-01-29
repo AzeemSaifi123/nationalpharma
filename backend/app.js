0b19a5f270b3fbf0ff425e596047b1371cb1f2c2
Azeem saifi <adkhan9355@gmail.com>
2025-01-29 14:51:55 +0530

    cors setup

--- backend/app.js
+++ backend/app.js
@@ -47,7 +47,7 @@ app.use((req,res,next)=>{
     next();
 });
 
-app.use(cors());
+// app.use(cors());
 
 const allowedOrigins = ['http://localhost:3000', 'https://thenationalpharma.com']; // Add your allowed origins
 
