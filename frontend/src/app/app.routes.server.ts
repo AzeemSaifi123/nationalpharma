4d462e81042db9630f1cd88ecfbeff8baee4e454
AzeemSaifi123 <adkhan9355@gmail.com>
2025-01-31 12:30:19 +0530

    cors update

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -4,7 +4,5 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
-  },
-  { path: 'ssr-route', renderMode: RenderMode.Server },
-  // { path: 'csr-route', renderMode: RenderMode.Client },
+  }
 ];
