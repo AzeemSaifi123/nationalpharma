784d3460a352d7c6ad637a853b44befad22b1a1c
Azeem saifi <adkhan9355@gmail.com>
2025-02-01 12:32:35 +0530

    ip changes

--- backend/app.js
+++ backend/app.js
@@ -39,7 +39,6 @@ mongoose.connect(uri)
     console.error('Error connecting to MongoDB:', error);
     // console.error('Mongoose query error:', error);
     res.status(500).json({ error: 'An error occurred' })
-    process.exit(1);
 });
 
 
