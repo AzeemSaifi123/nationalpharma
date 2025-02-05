a2097fe85fec2bf567efcd34a958bc055b9389b3
AzeemSaifi123 <adkhan9355@gmail.com>
2025-02-05 14:46:30 +0530

    dist

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
         production: true,
-          apiUrl:"https://api.thenationalpharma.com/database"
+          //apiUrl:"https://api.thenationalpharma.com/database"
         // apiUrl:"http://localhost:3000/database"
+         apiUrl:"http://147.93.96.8:3000/database"
 };
